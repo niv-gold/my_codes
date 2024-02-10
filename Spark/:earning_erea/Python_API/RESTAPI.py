4c5e73ee59136d06feb6024866126a786b53d977
Niv Goldberg <nivgoldberg1@gmail.com>
2024-02-10 08:22:33 +0200

    rest api practice

---
+++ Spark/:earning_erea/Python_API/RESTAPI.py
@@ -0,0 +1,11 @@
+import requests as req
+import pandas as pd
+
+# missions:
+# 1. connect to rest_api that holds movies data
+# 2. check the return information type: text, json..
+# 3. isolatated 5 attributes and save them in pandas dataframe
+# 4. clean the data: nulls, trim acces white spaces and more
+# 5. creat a connection to mysql DB
+# 6. store the data in the DB 
+# 7. query the data using SQL query that could be used as a base for a report. 
